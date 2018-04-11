633f4166310f0fd95876438b1bd2f3792c271e46
asdf <2065471054@qq.com>
2018-04-11 10:33:58 +0800

    老哥

--- src/com/bdqn/test/Test01.java
+++ src/com/bdqn/test/Test01.java
@@ -3,9 +3,6 @@ package com.bdqn.test;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("��1001���޸�");
-       
-		System.out.println("这是我写的");
-		System.out.println("�����޸���");
+		System.out.println("你是干什么的");
 	}
 }
