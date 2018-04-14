92e426fb9350f79aaf23c0bf947aec73cb068479
cyz <2227078853@qq.com>
2018-04-14 18:35:57 +0800

    添加了国内投保 添加方法

--- src/com/tangcco/mapper/InlandMapper.java
+++ src/com/tangcco/mapper/InlandMapper.java
@@ -1,5 +1,9 @@
+//by 陈亚洲
 package com.tangcco.mapper;
 
-public interface InlandMapper {
+import com.tangcco.pojo.Inland;
 
+public interface InlandMapper {
+	//国内投保(添加)
+	int insertInland(Inland inland);
 }
