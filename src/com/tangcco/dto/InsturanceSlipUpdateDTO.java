e3184d8f03ccc98141c692c55865f5acf61576e8
asdf <2065471054@qq.com>
2018-04-19 12:16:49 +0800

    asdf

--- src/com/tangcco/dto/InsturanceSlipUpdateDTO.java
+++ src/com/tangcco/dto/InsturanceSlipUpdateDTO.java
@@ -1,5 +1,23 @@
 package com.tangcco.dto;
 
 public class InsturanceSlipUpdateDTO {
-	private int bxType;
+	// 投保单号
+
+	// 保险金额
+
+	// 投保日期
+
+	// 运输工具名称
+
+	// 目的地名称
+
+	// 自定义查询编码
+
+	// 发票/标记
+
+	// 提单/运单号码
+
+	// 商业节点
+
+	// 单据状态
 }
